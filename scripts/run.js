6eea5668240c3498ede8f98fda2dc48204f7a954
mzawadi <mwaranguallan345@gmail.com>
2021-12-07 13:48:35 +0300

    update run.js

--- scripts/run.js
+++ scripts/run.js
@@ -1,25 +1,23 @@
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
+    // const [owner, randomPerson] = await hre.ethers.getSigners();
     const musicContractFactory = await hre.ethers.getContractFactory('MusicPortal');
     const musicContract = await musicContractFactory.deploy();
     await musicContract.deployed();
-
-    console.log("Contract deployed to:", musicContract.address);
-    console.log("Contract deployed by:", owner.address);
+    console.log('Contract address:', musicContract.address);
     
     let musicCount;
     musicCount = await musicContract.getTotalShazams();
+    console.log(musicCount.toNumber());
     
-    let musicTxn = await musicContract.shazam();
+    let musicTxn = await musicContract.shazam('https://www.youtube.com/watch?v=O6SeD1xE7NA&ab_channel=VPRecords');
     await musicTxn.wait();
 
-    var shazamer =  randomPerson;
-    console.log(shazamer.address);
-
-    musicTxn = await musicContract.connect(shazamer).shazam();
+    const [_, randomPerson] = await hre.ethers.getSigners();
+    musicTxn = await musicContract.connect(randomPerson).shazam('https://www.youtube.com/watch?v=O6SeD1xE7NA&ab_channel=VPRecords')
     await musicTxn.wait();
 
-    musicCount = await musicContract.getTotalShazams();
+    let allMusic = await musicContract.getAllShazams();
+    console.log(allMusic);
 };
   
   const runMain = async () => {
